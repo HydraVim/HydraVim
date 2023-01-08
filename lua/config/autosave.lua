0264473e9dc03b6aa646714eec953ef56a213243
washonrails <iamx.3ur@protonmail.com>
2023-01-08 13:43:26 -0300

    Markdown Plugin

--- lua/config/autosave.lua
+++ lua/config/autosave.lua
@@ -4,4 +4,6 @@ autocmd BufNewFile,BufRead *.js :autocmd TextChanged,TextChangedI <buffer> silen
 autocmd BufNewFile,BufRead *.css :autocmd TextChanged,TextChangedI <buffer> silent write
 autocmd BufNewFile,BufRead *.html :autocmd TextChanged,TextChangedI <buffer> silent write
 autocmd BufNewFile,BufRead *.php :autocmd TextChanged,TextChangedI <buffer> silent write
+autocmd BufNewFile,BufRead *.rb :autocmd TextChanged,TextChangedI <buffer> silent write
+autocmd BufNewFile,BufRead *.erb :autocmd TextChanged,TextChangedI <buffer> silent write
 ]])
